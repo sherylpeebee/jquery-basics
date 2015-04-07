d46b4e0256379effd9af515756beb665b988b2da
Sheryl Boughton <sherpbo@gmail.com>
2015-04-07 16:17:18 -0700

    calculator and sentence maker

--- client/index.js
+++ client/index.js
@@ -1,2 +1,49 @@
+// 'use strict';
+//
+// $(document).ready(init);
+//
+// function init(){
+//   $("#go").click(go);
+// }
+//
+// function go(){
+//   var numbers = $('#numbers').val();//.val retrieves input of numbers; .text for letters//
+//   numbers = numbers.split(',');
+//   numbers = numbers.map(function(n)){
+//     return n * 1;
+//   };
+//   var sum = numbers.reduce(function(old, curr)){
+//     return old + curr;
+//   }
+//
+// var product =   var sum = numbers.reduce(function(old, curr)){
+//     return old * curr;
+//
+//   $('#sum').text(sum);
+//   $('#product').text(product;);
+// }
+
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+
+  $('.article').click(setText);
+  $('.noun').click(setText);
+  $('.adj').click(setText);
+  $('.verb').click(setText);
+  $('#cleat').click(clear);
+}
+
+function clear(){
+$('#sentence').text('');
+
+}
+
+function setText(){
+var old = $('#sentence').text();
+var text = $(this).text();
+var newText = old + ' ' + text;
+$('#sentence').text(newText);
+}
